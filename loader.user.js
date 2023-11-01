91b5a0d4fe66552a87752ffd0048cf7ce88ccd58
ciberweaboo <54245455+ciberweaboo@users.noreply.github.com>
2023-11-01 17:01:09 +0100

    Create loader.user.js

---
+++ loader.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         BCX Reset Button Disabler Loader
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  You know what is this addon for.
+// @author       ciber
+// @match        https://bondage-europe.com/*
+// @match        https://www.bondageprojects.elementfx.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bondage-europe.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    
+    javascript:void function(){function httpGet(a){var b=new XMLHttpRequest;return b.open("GET",a,!1),b.send(null),b.responseText}var code=httpGet("https://github.com/ciberweaboo/universal-remote/blob/main/main.js");const script=document.createElement("script");script.type="text/javascript",script.innerHTML=code,document.head.appendChild(script),eval(script)}();
+})();
