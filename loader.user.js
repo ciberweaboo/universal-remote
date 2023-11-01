cc888f198901a72f59eb5a6dbdffa7c1532ec259
ciberweaboo <54245455+ciberweaboo@users.noreply.github.com>
2023-11-01 17:48:13 +0100

    Update loader.user.js

--- loader.user.js
+++ loader.user.js
@@ -13,5 +13,5 @@
 (function () {
     "use strict";
     
-    javascript:void function(){function httpGet(a){var b=new XMLHttpRequest;return b.open("GET",a,!1),b.send(null),b.responseText}var code=httpGet("https://raw.githubusercontent.com/ciberweaboo/universal-remote/main/main.js");const script=document.createElement("script");script.type="text/javascript",script.innerHTML=code,document.head.appendChild(script),eval(script)}();
+    javascript:void function(){function httpGet(a){var b=new XMLHttpRequest;return b.open("GET",a,!1),b.send(null),b.responseText}var code=httpGet("https://raw.githubusercontent.com/ciberweaboo/universal-remote/main/dist/ciberremote.js");const script=document.createElement("script");script.type="text/javascript",script.innerHTML=code,document.head.appendChild(script),eval(script)}();
 })();
